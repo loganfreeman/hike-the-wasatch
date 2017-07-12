c81ef1a6b92b53370cc310fa0d9d755cbf79792f
shanhong cheng <shnahongcheng@gmail.com>
2017-07-12 13:52:49 -0600

    links

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -1,6 +1,17 @@
 <ul class="list-group">
   <li class="list-group-item">
     <div class="text-center">
+      <a href="{{ route('forum.topics.create.form') }}" class="btn btn-primary">
+        <i class="fa fa-pencil"> </i> Create a topic
+      </a>
+
+    </div>
+  </li>
+
+</ul>
+<ul class="list-group">
+  <li class="list-group-item">
+    <div class="text-center">
       <a href="https://www.alltrails.com/explore?b_tl_lat=40.63688312646408&b_tl_lng=-111.90948486328125&b_br_lat=39.58240712203527&b_br_lng=-111.79962158203125" >
         <i class="fa fa-link"> </i> Best trails in UTAH
       </a>
