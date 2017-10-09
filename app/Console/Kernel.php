9e6607a18c7f84eccd47a0bfdada17ec968ecd1a
shanhong cheng <shnahongcheng@gmail.com>
2017-10-08 20:34:47 -0600

    CreateUser

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\UpdateUserRole::class,
+        Commands\CreateUser::class,
         Commands\ClearLocalAvatarStorage::class,
     ];
 
