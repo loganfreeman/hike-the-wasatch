1671d2e958c8365e011f7720d01d984ddc1d1912
shanhong cheng <shnahongcheng@gmail.com>
2017-10-09 21:13:50 -0600

    status

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -1,6 +1,9 @@
 <ul class="list-group">
   <li class="list-group-item">
     <div class="text-center">
+      <a href="{{ route('forum.topics.create.form') }}" class="btn btn-primary">
+        <i class="fa fa-pencil"> </i> Create a topic
+      </a>
       <a href="https://www.alltrails.com/explore?b_tl_lat=40.925964939514294&b_tl_lng=-114.02984619140625&b_br_lat=39.28967134685658&b_br_lng=-109.676513671875" class="btn btn-primary">
         <i class="fa fa-pencil"> </i> Best hikes in UTAH
       </a>
